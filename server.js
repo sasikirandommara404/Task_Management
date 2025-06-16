939b19c8f7495a9666162dc100313619692e21f0
Sasikiran Dommara <sasikirandommara404@gmail.com>
2025-06-17 01:38:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors())
 
 const fun = async ()=>{
     try{
-        await mongoose.connect("mongodb+srv://sasikiran9949:sasi9381@cluster0.psevtbo.mongodb.net/task_db")
+        await mongoose.connect("mongodb://127.0.0.1:27017/task_db")
         console.log('Connected SucessFully ')
         console.log(`server is running at http://localhost:${port}/user`)
 
